6309bd346a7a7cc2ff522679e40953993e247e6c
taz-dev <tazdev@naver.com>
2021-10-11 20:38:21 +0900

    feat: 지연 로딩과 조회 성능 최적화
    1. 엔티티를 직접 노출
    --> 2. 엔티티를 DTO로 변환
    3. 엔티티를 DTO로 변환 - 페치 조인 최적화
    4. JPA에서 DTO로 바로 조회

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * xToOne(ManyToOne, OneToOne) 관계 최적화
@@ -27,7 +32,7 @@ public class OrderSimpleApiController {
      * - 양방향 관계 문제 발생 -> @JsonIgnore
      */
     @GetMapping("/api/v1/simple-orders")
-    public List<Order> orderV1() {
+    public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
             order.getMember().getName(); //LAZY 강제 초기화
@@ -35,4 +40,49 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    /*
+     * V2. 엔티티를 조회해서 DTO 로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출 -> 성능 떨어짐
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //ORDER 2개
+        //N + 1 -> 1 + 회원 N + 배송 N -> 쿼리 5개
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+    
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getOrderStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
+    }
+
+    /*
+     * V3. 엔티티를 조회해서 DTO 로 변환(fetch join 사용O)
+     * - fetch join 으로 쿼리 1번 호출
+     */
+
+    /*
+     * V4. JPA 에서 DTO 로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하는 데이터만 선택해서 조회
+     */
 }
