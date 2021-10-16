90ca81e06f8eeb25372af31ed789042cc262d3c0
taz-dev <tazdev@naver.com>
2021-10-16 13:21:13 +0900

    feat: 컬렉션 조회 최적화
    1. 엔티티를 직접 노출
    2. 엔티티를 DTO로 변환
    3. 엔티티를 DTO로 변환 - 페치 조인 최적화
    3-1. 엔티티를 DTO로 변환 - 페이징과 한계 돌파
    --> 4. JPA에서 DTO 직접 조회
    5. JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화
    6. JPA에서 DTO 직접 조회 - 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     /*
     * 1. 엔티티를 직접 노출
@@ -90,6 +93,14 @@ public class OrderApiController {
                 .collect(Collectors.toList());
 
         return result;
+    }
+
+    /*
+    * 4. JPA에서 DTO 직접 조회
+    */
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
     }
 
     @Getter
